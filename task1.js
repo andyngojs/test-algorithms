d0f6ee77ec323868199822fd5c14ff91604ef694
andyngojs <dongngo.2000@gmail.com>
2021-06-29 20:24:28 +0700

    submit task 1

--- task1.js
+++ task1.js
@@ -1,14 +1,16 @@
 function check(arr, k) {
+    let isInclude = false
     for (var i = 0; i < arr.length; i++) {
-        if (arr[i] != k) 
-        continue
-
-        if (arr[i] == k ) {
-            console.log('có tồn tại')
-            break
+        if (arr[i] === k) {
+            isInclude = true
         }
-
+    }
+    if (isInclude) {
+        console.log('có tồn tại phần tử k = ' + k + ' trong mảng ' + arr)
+    } else {
+        console.log('không tồn tại phần tử k = ' + k + ' trong mảng ' + arr)
     }
 }
 
-check([1,2,3,4,5], 4)
+check([1,2,3,4,5], 3)
+check([1,2,3,4,5], 7)
