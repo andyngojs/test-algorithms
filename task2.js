958dbbfddbcc4d6cfdf9fd37d79f60a5374da8c4
andyngojs <dongngo.2000@gmail.com>
2021-06-29 15:37:49 +0700

    submit task 2

---
+++ task2.js
@@ -0,0 +1,11 @@
+// Check a number is prime
+
+function checkPrime(n) {
+    if (n % 2 != 0 ) {
+        console.log('is prime')
+    } else {
+        console.log('not prime')
+    }
+}
+
+checkPrime(5)
